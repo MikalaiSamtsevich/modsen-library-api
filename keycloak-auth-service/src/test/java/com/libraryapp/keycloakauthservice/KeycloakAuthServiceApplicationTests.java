c9a15784cc2b83c2c7b42213040539d1c3385f50
Mikalai <mikalai.samtsevich@gmail.com>
2024-09-27 01:40:25 +0300

    Add Keycloak Auth Service module and Keycloak to Docker Compose
    - Created a new module `keycloak-auth-service` to handle authentication and authorization.
    - Added Keycloak configuration to `docker-compose` for centralized identity management.

---
+++ keycloak-auth-service/src/test/java/com/libraryapp/keycloakauthservice/KeycloakAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryapp.keycloakauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
