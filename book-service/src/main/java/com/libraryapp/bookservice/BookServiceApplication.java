73fce46720a73c76dbe08c14e13ca401b981395b
Mikalai <mikalai.samtsevich@gmail.com>
2024-09-27 00:50:19 +0300

    Add Eureka service and configure clients for service discovery
    - Created a new `eureka-service` for centralized service discovery.
    - Configured the necessary Spring Boot clients to register with Eureka:
      - Updated `application.yml`/`application.properties` to enable client
       registration and discovery in each service.
      - Verified communication between services through Eureka for better
       scalability and dynamic service discovery.

--- book-service/src/main/java/com/libraryapp/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/libraryapp/bookservice/BookServiceApplication.java
@@ -2,10 +2,12 @@ package com.libraryapp.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
--- library-service/src/main/java/com/libraryapp/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/com/libraryapp/libraryservice/LibraryServiceApplication.java
@@ -2,10 +2,12 @@ package com.libraryapp.libraryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LibraryServiceApplication {
 
     public static void main(String[] args) {
