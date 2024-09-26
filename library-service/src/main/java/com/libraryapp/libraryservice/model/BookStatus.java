f2bd199874021aff14eba5be7e9249306c7020fe
Mikalai <mikalai.samtsevich@gmail.com>
2024-09-26 20:20:31 +0300

    Add BookStatus entity and migration for book_status table; Update docker-compose with new Postgres container for library-service
    Created the BookStatus entity to manage the status of books in the library.
    Added a Liquibase migration for the book_status table to support the new entity.
    Updated docker-compose.yml to include a new Postgres container for the library-service,
     ensuring proper database management and isolation for the service.

---
+++ library-service/src/main/java/com/libraryapp/libraryservice/model/BookStatus.java
@@ -0,0 +1,25 @@
+package com.libraryapp.libraryservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "book_status")
+public class BookStatus {
+    @Id
+    @Column(name = "book_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "borrowed_at", nullable = false)
+    private LocalDateTime borrowedAt;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDateTime dueDate;
+
+}
