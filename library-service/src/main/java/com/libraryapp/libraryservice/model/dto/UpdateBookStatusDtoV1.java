53ca1d7ea4840e69d0c1ca3aa2c73b5a754d1422
Mikalai <mikalai.samtsevich@gmail.com>
2024-10-04 23:13:33 +0300

    Add Postman collections for testing microservices - add Postman collections for testing keycloak-auth-service, book-service, and library-service in folder "postman".

--- library-service/src/main/java/com/libraryapp/libraryservice/model/dto/UpdateBookStatusDtoV1.java
+++ library-service/src/main/java/com/libraryapp/libraryservice/model/dto/UpdateBookStatusDtoV1.java
@@ -1,6 +1,8 @@
 package com.libraryapp.libraryservice.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Builder;
 import lombok.Value;
@@ -9,13 +11,17 @@ import java.time.LocalDateTime;
 
 @Value
 @Builder
+@Schema(description = "Data transfer object for updating the status of a book.")
 public class UpdateBookStatusDtoV1 {
 
     @PastOrPresent(message = "Borrowed date cannot be in the future")
+    @Schema(description = "The date when the book was borrowed. It cannot be in the future.", example = "2023-09-15T10:15:30")
     LocalDateTime borrowedAt;
 
     @Future(message = "Due date must be in the future")
+    @Schema(description = "The due date for returning the book. It must be in the future.", example = "2024-10-15T10:15:30")
     LocalDateTime dueDate;
 
+    @Schema(description = "Indicates if the book is currently available.", example = "true")
     Boolean isFree;
 }
